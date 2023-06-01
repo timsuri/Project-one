16df67528c0f630c76526f2a5b23c995e9bcae4b
unknown <timotej.suri@szud.local>
2023-06-01 11:42:21 +0200

    kinda done

--- Bar graph attempt.R
+++ Bar graph attempt.R
@@ -1,16 +1,17 @@
 # Load ggplot2
 library(ggplot2)
-library(readr)
-
 
 # Create data
 data <- read.csv("Data.csv")
-  
-#  data.frame(
-#  name=c("A","B","C","D","E") ,  
-#  value=c(3,12,5,18,45)
-#)
 
-# Barplot
-ggplot(data, aes(x=Week,y=Excess_mortality)) + 
-  geom_bar(stat = "identity")
+
+
+ggplot(data, aes(Week, mortality, group=Year)) +
+  geom_bar(stat = "identity", fill = "Red", width = 1) +
+  labs(x = "Week", y = "Excess mortality", title = "Bar Chart") +
+  theme_minimal() +
+  ylim(-0.1, 0.8) +
+  facet_grid(.~Year, scales = "free", switch = "x", space = "free_x") + 
+  theme(strip.placement = "outside", strip.text.x = element_text(margin = margin(0, 0, 0, 0)))+
+  scale_x_continuous(breaks = seq(1, 52, 2))+
+  scale_y_continuous(breaks = seq(-0.1, 0.8, 0.1))  
